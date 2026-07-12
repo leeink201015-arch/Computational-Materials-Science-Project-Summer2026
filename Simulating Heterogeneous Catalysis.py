74918d09896849175408db1397af909dd54bbda6
leeguan-chem <leeink201015@gmail.com>
2026-07-11 17:34:30 -0700

    Add files via upload
    Completed my code that simulates the disproportionation reaction of CO on metal surface catalyst and fixed a bunch of error from previous update

--- Simulating Heterogeneous Catalysis.py
+++ Simulating Heterogeneous Catalysis.py
@@ -73,5 +73,74 @@ fe_surface_with_co.to(fmt="cif", filename="fe_100_with_CO.cif")
 import os; os.system("explorer .")
 print(f"File saved in {os.getcwd()}")
 
-# Adsorption - basically add carbon and oxygen coordinates to our rearranged_sites
-rearranged_sites.append(c_coordinate[0])
+# Reaction going on
+# First, the CO needs to shift from directly above an Fe atom to the hollow site
+
+co_hollow_floating_distance = 0.80
+
+c_hollow_coordinate = [rearranged_sites[1].coords[0], rearranged_sites[1].coords[1], rearranged_sites[1].coords[2] + co_hollow_floating_distance]
+o_hollow_coordinate = [rearranged_sites[1].coords[0], rearranged_sites[1].coords[1], rearranged_sites[1].coords[2] + co_hollow_floating_distance + co_triple_bond_length]
+
+################################################################################
+
+slab_reaction_initial = slab_gen.get_slabs()
+fe_surface_reaction_initial = slab_reaction_initial[0]
+
+fe_surface_reaction_initial.append(species="O", coords=o_hollow_coordinate, coords_are_cartesian=True)
+fe_surface_reaction_initial.append(species="C", coords=c_hollow_coordinate, coords_are_cartesian=True)
+
+fe_surface_reaction_initial.append(species="O", coords=[2.87, 0.00, rearranged_sites[1].coords[2] + co_hollow_floating_distance], coords_are_cartesian=True)
+fe_surface_reaction_initial.append(species="C", coords=[2.87, 0.00, rearranged_sites[1].coords[2] + co_hollow_floating_distance + co_triple_bond_length], coords_are_cartesian=True)
+
+
+fe_surface_reaction_initial.to(fmt="cif", filename="reaction_process_initial.cif") # Not fe_surface reaction as the variables hve all been wiped out
+import os; os.system("explorer .")
+print(f"File saved in {os.getcwd()}")
+
+# Simulate bond dissociation and formation - disproportionation of CO
+
+# Reaction is CO(g) --> CO2(g) + C(ad)
+
+slab_reaction = slab_gen.get_slabs()
+fe_surface_reaction = slab_reaction[0]
+
+c_hollow_floating_distance = 0.50
+carbon_atom_adsorbed_coordinates = [rearranged_sites[1].coords[0], rearranged_sites[1].coords[1], rearranged_sites[1].coords[2] + c_hollow_floating_distance]
+
+fe_surface_reaction.append(species="C", coords=carbon_atom_adsorbed_coordinates, coords_are_cartesian=True)
+
+carbon_dioxide_floating_distance = 4.5
+
+co2_center_z = rearranged_sites[1].coords[2] + carbon_dioxide_floating_distance  # Floating safely above the surface
+
+co2_c_coordinates = [rearranged_sites[0].coords[0] + 2.87, rearranged_sites[0].coords[1], co2_center_z]                             # Carbon center
+co2_o1_coordinates = [rearranged_sites[0].coords[0] + 2.87, rearranged_sites[0].coords[1], co2_center_z - co_triple_bond_length]    # Bottom Oxygen
+co2_o2_coordinates = [rearranged_sites[0].coords[0] + 2.87, rearranged_sites[0].coords[1], co2_center_z + co_triple_bond_length]    # Top Oxygen
+
+# I set the (x,y) coordinates of the CO2 molecule to be (2.87, 0.0) because that's going to keep the movie running smoothly like a real simulation, setting it to (0,0) is a mismatch
+
+fe_surface_reaction.append(species="C", coords=co2_c_coordinates, coords_are_cartesian=True)
+fe_surface_reaction.append(species="O", coords=co2_o1_coordinates, coords_are_cartesian=True)
+fe_surface_reaction.append(species="O", coords=co2_o2_coordinates, coords_are_cartesian=True)
+
+fe_surface_reaction.to(fmt="cif", filename="reaction_process.cif")
+import os; os.system("explorer .")
+print(f"File saved in {os.getcwd()}")
+
+# Making the movie #
+
+def make_movie(start, end, steps = 5):
+    import os
+    
+    folder = "./reaction_movie"
+    os.makedirs(folder, exist_ok=True)
+    total = steps + 2
+    for idx in range(total):
+        frac = idx/(total - 1)
+        frame = start.copy()
+        for atom_idx in range(len(frame)):
+            frame [atom_idx].coords = start[atom_idx].coords + frac * (end[atom_idx].coords - start[atom_idx].coords)
+        frame.to("cif", filename=f"{folder}/frame_{idx}.cif")
+    print("Movie saved in {folder}")
+
+make_movie(fe_surface_reaction_initial, fe_surface_reaction)
